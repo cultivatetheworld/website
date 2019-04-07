cce4304be4e1b52c40bb67048eb51c012237bc15
Akash Bhave <akashbbhave@gmail.com>
2019-04-07 05:25:30 -0400

    Change fonts, create navbar
    Signed-off-by: Akash Bhave <akashbbhave@gmail.com>

---
+++ components/TheHeader.vue
@@ -0,0 +1,37 @@
+<template>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+        <b-navbar-brand href="#">
+            <img id="navbar-logo" src="~/assets/img/logo.png" alt="Logo">
+        </b-navbar-brand>
+        <b-navbar-brand href="#" class="text-uppercase" id="navbar-header">Cultivate the World</b-navbar-brand>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+            <b-navbar-nav class="ml-auto">
+                <b-nav-item to="/">Home</b-nav-item>
+                <b-nav-item to="/about">About</b-nav-item>
+                <b-nav-item to="/demo">Demo</b-nav-item>
+            </b-navbar-nav>
+        </b-collapse>
+    </b-navbar>
+</template>
+
+<script>
+export default {
+    title: "TheHeader"
+};
+</script>
+
+<style>
+.navbar {
+    background-color: #435058 !important;
+    font-family: "Imprima", sans-serif !important;
+}
+#navbar-logo {
+    max-height: 30px;
+}
+#navbar-header {
+    font-family: "Imprima";
+}
+</style>
